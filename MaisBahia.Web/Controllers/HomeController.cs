2c2cc9b65fcfcf46b90657abb4aa15acb57cd6f0
Helder Oliveira <rafaelsuporte-ti@hotmail.com>
2017-07-23 09:58:45 -0300

    teste
    teste

--- MaisBahia.Web/Controllers/HomeController.cs
+++ MaisBahia.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaisBahia.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,12 @@ namespace MaisBahia.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private MaisBahiaContext db = new MaisBahiaContext();
         public ActionResult Index()
         {
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "Id", "Nome");
+            ViewBag.FotoId = new SelectList(db.Fotos, "Id", "Titulo");
+            ViewBag.PlanoId = new SelectList(db.Planos, "Id", "Nome");
             return View();
         }
 
